e73ab1e3a36d50df658d7740662e3b9f30a98a25
joon0725 <joon0725@gmail.com>
2022-07-13 21:00:30 +0900

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import requests
 
 app = Flask(__name__)
-api_url = "https://graph.facebook.com/v13.0/me/messages"
+api_url = "https://graph.facebook.com/v14.0/me/messages"
 v_token = "bipaktfmpea"
 acc_token = "EAAGf9UtML2gBAJFrfidEGGL37xJiegdiIzsgKRZAZCY5PwQPxZAO6T5KkK7ZANkCpk5a9FZCUS9GKyX4r3yyXy6zXLGAuzcqhafRhcyF1Fpcj1yKsPu4fC1v4GtJcFSQSngegAZB6chXEenDyBICdKIrg14Cg58yhnyssn7Ajee9q1t22ZAg1EU"
 
