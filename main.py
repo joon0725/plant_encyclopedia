61436cee2d98f5858791b3551f5b4e2fac9b7ac1
joon0725 <joon0725@gmail.com>
2022-07-13 21:56:09 +0900

    update

--- main.py
+++ main.py
@@ -7,10 +7,55 @@ v_token = "bipaktfmpea"
 with open("/workspace/plant-encyclopedia/token_acc", 'r') as f:
     acc_token = f.read()
 
+session = {}
+
 
 def get_bot_response(sender, message):
-    if message == "야":
-        return "네"
+    global session
+    if message == "시작하기":
+        session[sender] = {"session": 1, "color": None}
+        return "꽃 색은 무슨색입니까? (흰색, 분홍색, 녹색, 주황색, 보라색, 노란색, 갈색 중 택 1)"
+    if session[sender]["session"] == 1 and session[sender]["color"] is None:
+        if "흰색" in message:
+            session[sender]["color"] = 1
+            return "꽃이 핀 형태는 어떠한가요? (무리, 일렬)"
+        elif "분홍색" in message:
+            session[sender]["color"] = 2
+        elif "녹색" in message:
+            session[sender]["color"] = 3
+        elif "주황색" in message:
+            session[sender]["color"] = 4
+            session[sender] = {"session": None, "color": None}
+            return "그 꽃은 인도 칸나입니다."
+        elif "보라색" in message:
+            session[sender]["color"] = 5
+            session[sender] = {"session": None, "color": None}
+            return "그 꽃은 순비기나무입니다."
+        elif "노란색" in message:
+            session[sender]["color"] = 6
+            session[sender] = {"session": None, "color": None}
+            return "그 꽃은 인동덩굴입니다."
+        elif "갈색" in message:
+            session[sender]["color"] = 7
+            session[sender] = {"session": None, "color": None}
+            return "그 꽃은 밀사초입니다."
+        else:
+            return "저 중에 다시 선택해 주세요"
+        return "잎 모양은 어떠한가요? (물방울, 단풍, 길쭉함, 가늘고 길쭉 중 택 1)"
+    if session[sender]["session"] == 1 and session[sender]["color"] is not None:
+        session[sender] = {"session": None, "color": None}
+        if session[sender]["color"] == 1 and "무리" in message:
+            return "그 꽃은 사상자입니다."
+        elif session[sender]["color"] == 1 and "일렬" in message:
+            return "그 꽃은 참새피입니다."
+        elif session[sender]["color"] == 2 and "물방울" in message:
+            return "그 꽃은 분꽃입니다."
+        elif session[sender]["color"] == 2 and "길쭉함" in message:
+            return "그 꽃은 엉겅퀴입니다."
+        elif session[sender]["color"] == 3 and "단풍" in message:
+            return "그 꽃은 환삼덩굴입니다."
+        elif session[sender]["color"] == 3 and "가늘고 길쭉" in message:
+            return "그 꽃은 가는갯는쟁이입니다."
 
 
 def send_message(recipient_id, text):
